a021842aca3e7d041b5bb50a3abe6a035250e13b
pademinune <justinnitoi@gmail.com>
2026-02-18 17:44:43 -0500

    implemented the catalog generation

--- app.py
+++ app.py
@@ -1,47 +1,74 @@
 
 from flask import Flask
 from flask_cors import CORS
+import tmdb
+
 
 app = Flask(__name__)
 CORS(app)
 
+CATALOG_SIZE = 10
+
+BASE_URL = "http://127.0.0.1:7000"
+
 MANIFEST = {
-    "id": "org.stremio.python_example",
+    "id": "movie.genre.catalogs",
     "name": "Movie Genre Catalogs",
     "description": "Get more catalogs.",
-    "version": "1.1.0",
+    "version": "1.0.0",
     "resources": ["catalog"],
     "types": ["movie"],
-    "catalogs": [{"type": "movie", "id": "python_movies", "name": "Python Movies"}]
+    "catalogs": [{"type": "movie", "id": "action_movies", "name": "Action Movies"},
+                 {"type": "movie", "id": "adventure_movies", "name": "Adventure Movies"},
+                 {"type": "movie", "id": "comedy_movies", "name": "Comedy Movies"},
+                 {"type": "movie", "id": "drama_movies", "name": "Drama Movies"},
+                 {"type": "movie", "id": "fantasy_movies", "name": "Fantasy Movies"},
+                 {"type": "movie", "id": "horror_movies", "name": "Horror Movies"}
+                 ],
+    "logo": f"{BASE_URL}/static/img/favicon.png"
 }
-
-# MANIFEST = {
-#     "id": "movie.trailers.justin",
-#     "name": "Movie Trailers",
-#     "description": "Watch movie trailers.",
-#     "version": "1.1.0",
-#     "resources": ["stream"],
-#     "types": ["movie"],
-#     "catalogs": [],
-#     "idPrefixes": ["tt"],
-# }
 
 @app.route('/manifest.json')
 def manifest():
     return MANIFEST
 
-@app.route('/catalog/movie/python_movies.json')
-def catalog():
-    # This is the "row" content
-    metas = [
-        {
-            "id": "tt0111161", # IMDb ID
-            "type": "movie",
-            "name": "The Shawshank Redemption",
-            "poster": "https://images.metahub.space",
-        }
-    ]
+@app.route('/catalog/movie/action_movies.json')
+def action_catalog():
+    movies = tmdb.get_movies("Action", CATALOG_SIZE)
+    metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
+
+@app.route('/catalog/movie/adventure_movies.json')
+def adventure_catalog():
+    movies = tmdb.get_movies("Adventure", CATALOG_SIZE)
+    metas = [movie.to_dict() for movie in movies]
+    return {"metas": metas}
+
+@app.route('/catalog/movie/comedy_movies.json')
+def comedy_catalog():
+    movies = tmdb.get_movies("Comedy", CATALOG_SIZE)
+    metas = [movie.to_dict() for movie in movies]
+    return {"metas": metas}
+
+@app.route('/catalog/movie/drama_movies.json')
+def drama_catalog():
+    movies = tmdb.get_movies("Drama", CATALOG_SIZE)
+    metas = [movie.to_dict() for movie in movies]
+    return {"metas": metas}
+
+@app.route('/catalog/movie/fantasy_movies.json')
+def fantasy_catalog():
+    movies = tmdb.get_movies("Fantasy", CATALOG_SIZE)
+    metas = [movie.to_dict() for movie in movies]
+    return {"metas": metas}
+
+@app.route('/catalog/movie/horror_movies.json')
+def horror_catalog():
+    movies = tmdb.get_movies("Horror", CATALOG_SIZE)
+    metas = [movie.to_dict() for movie in movies]
+    return {"metas": metas}
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=7000, debug=True)
---
+++ tmdb.py
@@ -0,0 +1,75 @@
+
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TMDB_KEY = os.getenv("TMDB_KEY")
+
+GENRE_IDS = {"Action": 28, "Adventure": 12, "Animation": 16,
+             "Comedy": 35, "Crime": 80, "Documentary": 99,
+             "Drama": 18, "Family": 10751, "Fantasy": 14,
+             "History": 36, "Horror": 27, "Music": 10402,
+             "Mystery": 9648, "Romance": 10749,
+             "Science-Fiction": 878, "TV-Movie": 10770,
+             "Thriller": 53, "War": 10752, "Western": 37
+             }
+
+
+class Movie:
+    def __init__(self, id: str, name: str, 
+                 poster: str = "http://127.0.0.1:7000/static/img/9.jpg"):
+        self.id = id # https://api.themoviedb.org/3/movie/{tmdb_id}/external_ids?api_key={TMDB_KEY}
+        self.name = name
+        self.poster = poster # https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg
+
+    def to_dict(self) -> dict[str, str]:
+        return {"id": self.id, "type": "movie", 
+                "name": self.name, "poster": self.poster}
+    
+    def __str__(self) -> str:
+        return str(self.to_dict())
+
+
+def get_imdb_id(tmdb_id: int) -> str:
+    query = f"https://api.themoviedb.org/3/movie/{tmdb_id}/external_ids?api_key={TMDB_KEY}"
+    response = requests.get(query)
+    response_dict = response.json()
+    return response_dict["imdb_id"]
+
+def get_poster_url(short_url: str) -> str:
+    return f"https://image.tmdb.org/t/p/w500{short_url}"
+
+def get_movies(genre: str, amount: int = 20):
+    genre_id = GENRE_IDS[genre]
+    movies = []
+    page_number = 1
+
+    while (len(movies) < amount):
+        query = f"https://api.themoviedb.org/3/discover/movie?api_key={TMDB_KEY}&language=en-US&sort_by=popularity.desc&page={page_number}&with_genres={genre_id}"
+        response = requests.get(query)
+        response_dict = response.json()
+        response_movies = response_dict["results"]
+
+        if (len(movies) + len(response_movies) <= amount):
+            movies += response_movies
+        else:
+            movies += response_movies[0:(amount - len(movies))]
+    
+    movie_objects = []
+    for movie in movies:
+        imdb_id = get_imdb_id(movie["id"])
+        name = movie["title"]
+        poster_url = get_poster_url(movie["poster_path"])
+        movie_objects.append(Movie(imdb_id, name, poster_url))
+
+    return movie_objects
+
+
+if __name__ == "__main__":
+    movies = get_movies("Horror", 50)
+    for movie in movies:
+        print(movie)
+
