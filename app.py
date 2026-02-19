6429caaaa6a95269537ab75c0d6ac0c938851a02
pademinune <justinnitoi@gmail.com>
2026-02-19 16:12:57 -0500

    added html homepage

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from flask_caching import Cache
 import tmdb
@@ -40,47 +40,50 @@ app.config.from_mapping({
 
 cache = Cache(app)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/manifest.json')
+@app.route("/manifest.json")
 def manifest():
     return MANIFEST
 
-@app.route('/catalog/movie/action_movies.json')
+@app.route("/catalog/movie/action_movies.json")
 @cache.cached()
 def action_catalog():
     movies = tmdb.get_movies("Action", CATALOG_SIZE)
     metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
 
-@app.route('/catalog/movie/adventure_movies.json')
+@app.route("/catalog/movie/adventure_movies.json")
 @cache.cached()
 def adventure_catalog():
     movies = tmdb.get_movies("Adventure", CATALOG_SIZE)
     metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
 
-@app.route('/catalog/movie/comedy_movies.json')
+@app.route("/catalog/movie/comedy_movies.json")
 @cache.cached()
 def comedy_catalog():
     movies = tmdb.get_movies("Comedy", CATALOG_SIZE)
     metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
 
-@app.route('/catalog/movie/drama_movies.json')
+@app.route("/catalog/movie/drama_movies.json")
 @cache.cached()
 def drama_catalog():
     movies = tmdb.get_movies("Drama", CATALOG_SIZE)
     metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
 
-@app.route('/catalog/movie/fantasy_movies.json')
+@app.route("/catalog/movie/fantasy_movies.json")
 @cache.cached()
 def fantasy_catalog():
     movies = tmdb.get_movies("Fantasy", CATALOG_SIZE)
     metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
 
-@app.route('/catalog/movie/horror_movies.json')
+@app.route("/catalog/movie/horror_movies.json")
 @cache.cached()
 def horror_catalog():
     movies = tmdb.get_movies("Horror", CATALOG_SIZE)
@@ -89,5 +92,5 @@ def horror_catalog():
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7000, debug=True)
