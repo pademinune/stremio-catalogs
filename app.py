0e763d328654d26d2ca3abe1661a3a7cb5863af9
pademinune <justinnitoi@gmail.com>
2026-02-19 15:49:05 -0500

    added caching using redis

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 
 from flask import Flask
 from flask_cors import CORS
+from flask_caching import Cache
 import tmdb
 from dotenv import load_dotenv
 import os
-
-app = Flask(__name__)
-CORS(app)
 
 CATALOG_SIZE = 10
 
@@ -30,41 +28,60 @@ MANIFEST = {
     "logo": f"{BASE_URL}/static/img/favicon.png"
 }
 
+
+app = Flask(__name__)
+CORS(app)
+
+app.config.from_mapping({
+    "CACHE_TYPE": "RedisCache",
+    "CACHE_REDIS_URL": os.environ.get("REDIS_URL"),
+    "CACHE_DEFAULT_TIMEOUT": 86400  # 24-hour default
+})
+
+cache = Cache(app)
+
+
 @app.route('/manifest.json')
 def manifest():
     return MANIFEST
 
 @app.route('/catalog/movie/action_movies.json')
+@cache.cached()
 def action_catalog():
     movies = tmdb.get_movies("Action", CATALOG_SIZE)
     metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
 
 @app.route('/catalog/movie/adventure_movies.json')
+@cache.cached()
 def adventure_catalog():
     movies = tmdb.get_movies("Adventure", CATALOG_SIZE)
     metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
 
 @app.route('/catalog/movie/comedy_movies.json')
+@cache.cached()
 def comedy_catalog():
     movies = tmdb.get_movies("Comedy", CATALOG_SIZE)
     metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
 
 @app.route('/catalog/movie/drama_movies.json')
+@cache.cached()
 def drama_catalog():
     movies = tmdb.get_movies("Drama", CATALOG_SIZE)
     metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
 
 @app.route('/catalog/movie/fantasy_movies.json')
+@cache.cached()
 def fantasy_catalog():
     movies = tmdb.get_movies("Fantasy", CATALOG_SIZE)
     metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
 
 @app.route('/catalog/movie/horror_movies.json')
+@cache.cached()
 def horror_catalog():
     movies = tmdb.get_movies("Horror", CATALOG_SIZE)
     metas = [movie.to_dict() for movie in movies]
