33e679ae8cd6cd130442549c1547d23271ddc852
Justin <justinnitoi@gmail.com>
2026-04-27 12:01:25 -0400

    reformatted code with black

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from flask_caching import Cache
@@ -21,17 +20,48 @@ MANIFEST = {
     "version": "1.2.0",
     "resources": ["catalog"],
     "types": ["movie"],
-    "catalogs": [{"type": "movie", "id": "action_movies", "name": "Action", "extra": [{"name": "skip"}]},
-                 {"type": "movie", "id": "adventure_movies", "name": "Adventure", "extra": [{"name": "skip"}]},
-                 {"type": "movie", "id": "comedy_movies", "name": "Comedy", "extra": [{"name": "skip"}]},
-                 {"type": "movie", "id": "drama_movies", "name": "Drama", "extra": [{"name": "skip"}]},
-                 {"type": "movie", "id": "fantasy_movies", "name": "Fantasy", "extra": [{"name": "skip"}]},
-                 {"type": "movie", "id": "horror_movies", "name": "Horror", "extra": [{"name": "skip"}]},
-                #  {"type": "movie", "id": "sub_100_minutes", "name": "Movies Under 100 minutes", "extra": [{"name": "skip"}]},
-                #  {"type": "movie", "id": "documentaries", "name": "Documentaries", "extra": [{"name": "skip"}]},
-                #  {"type": "movie", "id": "random_movie", "name": "Random Movie"}
-                 ],
-    "logo": f"{BASE_URL}/static/img/favicon.png"
+    "catalogs": [
+        {
+            "type": "movie",
+            "id": "action_movies",
+            "name": "Action",
+            "extra": [{"name": "skip"}],
+        },
+        {
+            "type": "movie",
+            "id": "adventure_movies",
+            "name": "Adventure",
+            "extra": [{"name": "skip"}],
+        },
+        {
+            "type": "movie",
+            "id": "comedy_movies",
+            "name": "Comedy",
+            "extra": [{"name": "skip"}],
+        },
+        {
+            "type": "movie",
+            "id": "drama_movies",
+            "name": "Drama",
+            "extra": [{"name": "skip"}],
+        },
+        {
+            "type": "movie",
+            "id": "fantasy_movies",
+            "name": "Fantasy",
+            "extra": [{"name": "skip"}],
+        },
+        {
+            "type": "movie",
+            "id": "horror_movies",
+            "name": "Horror",
+            "extra": [{"name": "skip"}],
+        },
+        #  {"type": "movie", "id": "sub_100_minutes", "name": "Movies Under 100 minutes", "extra": [{"name": "skip"}]},
+        #  {"type": "movie", "id": "documentaries", "name": "Documentaries", "extra": [{"name": "skip"}]},
+        #  {"type": "movie", "id": "random_movie", "name": "Random Movie"}
+    ],
+    "logo": f"{BASE_URL}/static/img/favicon.png",
 }
 
 ID_TO_GENRE = {
@@ -47,79 +77,91 @@ ID_TO_GENRE = {
 app = Flask(__name__)
 CORS(app)
 
-app.config.from_mapping({
-    "CACHE_TYPE": "RedisCache",
-    "CACHE_REDIS_URL": os.environ.get("REDIS_URL"),
-    "CACHE_DEFAULT_TIMEOUT": 86400  # 24-hour default
-})
+app.config.from_mapping(
+    {
+        "CACHE_TYPE": "RedisCache",
+        "CACHE_REDIS_URL": os.environ.get("REDIS_URL"),
+        "CACHE_DEFAULT_TIMEOUT": 86400,  # 24-hour default
+    }
+)
 
 cache = Cache(app)
+
 
 def get_page_num(skip: int) -> int:
     """find page st 20 * page >= skip and page is minimal"""
     return math.ceil(skip / 20) + 1
 
+
 def get_cache_key(catalog_id: str, page: int = 1) -> str:
     return f"{catalog_id}_page={page}"
 
+
 def get_skip_from_args(args: str | None) -> int:
     skip = 0
-    if args and 'skip=' in args:
+    if args and "skip=" in args:
         try:
-            skip = int(args.split('=')[1])
+            skip = int(args.split("=")[1])
         except ValueError:
             skip = 0
     return skip
 
+
 def fetch_movie_genre(catalog_id: str, page: int = 0):
     genre = ID_TO_GENRE[catalog_id]
-    
+
     movies = tmdb.get_movies(genre, CATALOG_SIZE, start_page=page)
     metas = [movie.to_dict() for movie in movies]
     response = {"metas": metas}
     return response
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/manifest.json")
 def manifest():
     return MANIFEST
 
-@app.route('/catalog/movie/random_movie.json')
+
+@app.route("/catalog/movie/random_movie.json")
 def random_movie():
     movie = tmdb.get_random_movie()
     return {"metas": [movie.to_dict()]}
 
-@app.route('/catalog/movie/sub_100_minutes/<path:args>.json')
+
+@app.route("/catalog/movie/sub_100_minutes/<path:args>.json")
 @app.route("/catalog/movie/sub_100_minutes.json")
 def sub_100_minutes(args=None):
     skip = 0
-    if args and 'skip=' in args:
+    if args and "skip=" in args:
         try:
-            skip = int(args.split('=')[1])
+            skip = int(args.split("=")[1])
         except ValueError:
             skip = 0
 
     page = skip // 20 + 1
-    if (skip % 20 != 0): # it was rounded down, so add 1
+    if skip % 20 != 0:  # it was rounded down, so add 1
         page += 1
-    
-    movies = tmdb.get_movies_filtered({"with_runtime.lte": 100, "with_runtime.gte": 60},
-                                       CATALOG_SIZE, start_page=page)
+
+    movies = tmdb.get_movies_filtered(
+        {"with_runtime.lte": 100, "with_runtime.gte": 60}, CATALOG_SIZE, start_page=page
+    )
     metas = [movie.to_dict() for movie in movies]
     return {"metas": metas}
 
-@app.route('/catalog/movie/<catalog_id>/<path:args>.json')
-@app.route('/catalog/movie/<catalog_id>.json')
-def movie_catalog(catalog_id, args = None):    
+
+@app.route("/catalog/movie/<catalog_id>/<path:args>.json")
+@app.route("/catalog/movie/<catalog_id>.json")
+def movie_catalog(catalog_id, args=None):
     skip = get_skip_from_args(args)
     page = get_page_num(skip=skip)
 
     cache_key = get_cache_key(catalog_id, page)
     cached = cache.get(cache_key)
-    if (cached):
+    if cached:
         return cached
 
     response = fetch_movie_genre(catalog_id, page)
@@ -127,21 +169,24 @@ def movie_catalog(catalog_id, args = None):
     cache.set(cache_key, response)
     return response
 
+
 @app.route("/cache/refresh", methods=["POST"])
 def refresh_cache_endpoint():
     token = request.headers.get("X-Cron-Secret")
     if token != CRON_SECRET:
         return "Unauthorized", 401
-    
+
     thread = threading.Thread(target=refresh_cache)
     thread.start()
     return "Cache refresh started."
+
 
 def refresh_cache():
     for catalog_id in ID_TO_GENRE.keys():
         response = fetch_movie_genre(catalog_id, page=1)
         cache_key = get_cache_key(catalog_id, page=1)
         cache.set(cache_key, response)
+
 
 @app.route("/cache/reset", methods=["POST"])
 @app.route("/cache/clear", methods=["POST"])
@@ -154,8 +199,10 @@ def clear_cache_endpoint():
     thread.start()
     return "Cache clear started."
 
+
 def clear_cache():
     cache.clear()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7000, debug=True)
--- tmdb.py
+++ tmdb.py
@@ -1,4 +1,3 @@
-
 import requests
 import json
 from dotenv import load_dotenv
@@ -10,64 +9,97 @@ load_dotenv()
 TMDB_KEY = os.getenv("TMDB_KEY")
 BASE_URL = os.getenv("BASE_URL")
 
-GENRE_IDS = {"Action": 28, "Adventure": 12, "Animation": 16,
-             "Comedy": 35, "Crime": 80, "Documentary": 99,
-             "Drama": 18, "Family": 10751, "Fantasy": 14,
-             "History": 36, "Horror": 27, "Music": 10402,
-             "Mystery": 9648, "Romance": 10749,
-             "Science-Fiction": 878, "TV-Movie": 10770,
-             "Thriller": 53, "War": 10752, "Western": 37,
-             "Documentary": 99,
-             }
+GENRE_IDS = {
+    "Action": 28,
+    "Adventure": 12,
+    "Animation": 16,
+    "Comedy": 35,
+    "Crime": 80,
+    "Documentary": 99,
+    "Drama": 18,
+    "Family": 10751,
+    "Fantasy": 14,
+    "History": 36,
+    "Horror": 27,
+    "Music": 10402,
+    "Mystery": 9648,
+    "Romance": 10749,
+    "Science-Fiction": 878,
+    "TV-Movie": 10770,
+    "Thriller": 53,
+    "War": 10752,
+    "Western": 37,
+    "Documentary": 99,
+}
 
 
 class Movie:
-    def __init__(self, id: str, name: str, 
-                 poster: str = "http://127.0.0.1:7000/static/img/9.jpg"):
-        self.id = id # https://api.themoviedb.org/3/movie/{tmdb_id}/external_ids?api_key={TMDB_KEY}
+    def __init__(
+        self, id: str, name: str, poster: str = "http://127.0.0.1:7000/static/img/9.jpg"
+    ):
+        self.id = id  # https://api.themoviedb.org/3/movie/{tmdb_id}/external_ids?api_key={TMDB_KEY}
         self.name = name
-        self.poster = poster # https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg
+        self.poster = (
+            poster  # https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg
+        )
 
     def to_dict(self) -> dict[str, str]:
-        return {"id": self.id, "type": "movie", 
-                "name": self.name, "poster": self.poster}
-    
+        return {
+            "id": self.id,
+            "type": "movie",
+            "name": self.name,
+            "poster": self.poster,
+        }
+
     def __str__(self) -> str:
         return str(self.to_dict())
+
 
 def is_valid_movie(movie) -> bool:
     return (movie["poster_path"] is not None) and (movie["id"] is not None)
 
+
 def get_imdb_id(tmdb_id: int) -> str:
-    query = f"https://api.themoviedb.org/3/movie/{tmdb_id}/external_ids?api_key={TMDB_KEY}"
+    query = (
+        f"https://api.themoviedb.org/3/movie/{tmdb_id}/external_ids?api_key={TMDB_KEY}"
+    )
     response = requests.get(query)
     response_dict = response.json()
     return response_dict["imdb_id"]
 
+
 def get_poster_url(short_url: str) -> str:
-    if (not short_url):
+    if not short_url:
         return f"{BASE_URL}/static/img/poster_placeholder.png"
     return f"https://image.tmdb.org/t/p/w500{short_url}"
 
-def get_movies(genre: str, amount: int = 20, start_page = 1) -> list[Movie]:
+
+def get_movies(genre: str, amount: int = 20, start_page=1) -> list[Movie]:
     genre_id = GENRE_IDS[genre]
     movies = []
     page_number = start_page
 
-    while (len(movies) < amount):
+    while len(movies) < amount:
         # query = f"https://api.themoviedb.org/3/discover/movie?api_key={TMDB_KEY}&sort_by=popularity.desc&page={page_number}&with_genres={genre_id}"
         url = "https://api.themoviedb.org/3/discover/movie"
-        response = requests.get(url, params={"api_key": TMDB_KEY, "sort_by": "popularity.desc", 
-                                             "page": page_number, "with_genres": genre_id})
+        response = requests.get(
+            url,
+            params={
+                "api_key": TMDB_KEY,
+                "sort_by": "popularity.desc",
+                "page": page_number,
+                "with_genres": genre_id,
+            },
+        )
         response_dict = response.json()
         response_movies = response_dict["results"]
 
-        if (len(movies) + len(response_movies) <= amount):
+        if len(movies) + len(response_movies) <= amount:
             movies += response_movies
         else:
-            movies += response_movies[0:(amount - len(movies))]
+            movies += response_movies[0 : (amount - len(movies))]
         page_number += 1
-    
+
     movie_objects = []
     for movie in movies:
         imdb_id = get_imdb_id(movie["id"])
@@ -77,24 +109,32 @@ def get_movies(genre: str, amount: int = 20, start_page = 1) -> list[Movie]:
 
     return movie_objects
 
-def get_movies_filtered(filters: dict[str, str|int], amount: int = 20, start_page = 1) -> list[Movie]:
+
+def get_movies_filtered(
+    filters: dict[str, str | int], amount: int = 20, start_page=1
+) -> list[Movie]:
     movies = []
     page_number = start_page
 
-    while (len(movies) < amount):
+    while len(movies) < amount:
         # query = f"https://api.themoviedb.org/3/discover/movie?api_key={TMDB_KEY}&sort_by=popularity.desc&page={page_number}&with_genres={genre_id}"
         url = "https://api.themoviedb.org/3/discover/movie"
-        response = requests.get(url, params=({"api_key": TMDB_KEY, "sort_by": "popularity.desc", 
-                                             "page": page_number} | filters))
+        response = requests.get(
+            url,
+            params=(
+                {"api_key": TMDB_KEY, "sort_by": "popularity.desc", "page": page_number}
+                | filters
+            ),
+        )
         response_dict = response.json()
         response_movies = response_dict["results"]
 
-        if (len(movies) + len(response_movies) <= amount):
+        if len(movies) + len(response_movies) <= amount:
             movies += response_movies
         else:
-            movies += response_movies[0:(amount - len(movies))]
+            movies += response_movies[0 : (amount - len(movies))]
         page_number += 1
-    
+
     movie_objects = []
     for movie in movies:
         imdb_id = get_imdb_id(movie["id"])
@@ -103,17 +143,20 @@ def get_movies_filtered(filters: dict[str, str|int], amount: int = 20, start_pag
         movie_objects.append(Movie(imdb_id, name, poster_url))
 
     return movie_objects
+
 
 def get_random_movie() -> Movie:
     url = "https://api.themoviedb.org/3/discover/movie"
     page = random.randint(1, 25)
-    response = requests.get(url, params={"api_key": TMDB_KEY, "sort_by": "popularity.desc", "page": page}).json()
+    response = requests.get(
+        url, params={"api_key": TMDB_KEY, "sort_by": "popularity.desc", "page": page}
+    ).json()
     response_movies = response["results"]
 
     movie = random.choice(response_movies)
-    if (not is_valid_movie(movie)):
+    if not is_valid_movie(movie):
         return get_random_movie()
-    
+
     imdb_id = get_imdb_id(movie["id"])
     name = movie["title"]
     poster_url = get_poster_url(movie["poster_path"])
@@ -129,4 +172,3 @@ if __name__ == "__main__":
     print(movie)
     # for movie in movies:
     #     print(movie)
-
